4d1072398d0ac6690fdf8f7decb5421cd7845b68
HDZTony <954504788@qq.com>
2015-11-19 23:54:43 +0800

    Initial Commit

---
+++ 微信例子/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  微信例子
+//
+//  Created by 何东洲 on 15/11/19.
+//  Copyright © 2015年 何东洲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 微信例子/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  微信例子
+//
+//  Created by 何东洲 on 15/11/19.
+//  Copyright © 2015年 何东洲. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
